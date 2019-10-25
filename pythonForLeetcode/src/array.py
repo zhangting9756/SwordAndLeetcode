7ac816771964b87be8579ca01559b106f81b7030
zhangting9756 <tingzhangguo@163.com>
2019-10-25 21:14:52 +0800

    增加数组

--- pythonForLeetcode/src/array.py
+++ pythonForLeetcode/src/array.py
@@ -1,7 +1,22 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
-
-class Solution(object):
+"""买卖股票的最佳时机 II"""        
+class Solution1(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxProfit = 0
+        if(prices):
+            for i in range(len(prices)-1):
+                if(prices[i+1]>prices[i]):
+                    maxProfit += prices[i+1]-prices[i]
+            return maxProfit
+        else:
+            return 0
+"""从排序数组中删除重复项 """
+class Solution2(object):
     def removeDuplicates(self,nums):
         """
         :type nums: List[int]
@@ -18,23 +33,46 @@ class Solution(object):
             return count
         else:
             return 0
-        
-class Solution1(object):
-    def maxProfit(self, prices):
+class Solution3:
+    def rotate(self, nums, k):
         """
-        :type prices: List[int]
-        :rtype: int
+        Do not return anything, modify nums in-place instead.
         """
-        maxProfit = 0
-        if(prices):
-            for i in range(len(prices)-1):
-                if(prices[i+1]>prices[i]):
-                    maxProfit += prices[i+1]-prices[i]
-            return maxProfit
+        if nums:
+            k = k % len(nums)
+            nums[:]=nums[-k:]+nums[:-k]
+""" 存在重复"""
+class Solution4:
+    def containsDuplicate(self, nums):
+        a = len(nums)
+        b = len(set(nums))
+        if (a!=b):
+            return True
         else:
-            return 0
+            return False
+        
+"""只出现一次的数字 """
+class Solution5:
+    def singleNumber(self, nums): 
+        res = 0
+        for i in nums:
+            res^=i
+        return res
+"""   两个数组的交集 II"""    
+class Solution6:
+    def intersect(self, nums1, nums2): 
+        if (nums1):
+            if(nums2):
+                ret =[]
+                ret = [ i for i in nums1 if i in nums2 ]
+        return ret                 
+           
 if __name__ == '__main__':
-    a=[7,6,4,3,1]
-    t=Solution1()
-    print(t.maxProfit(a))   
-    print(a)    #a已经改变[1, 2, 2]
+    a=[1,3,2,4,5,3]
+    b=[2,3,7,8,3]
+    c=Solution6()
+    d=c.intersect(a,b)
+    print(d)
+    
+    
+ 
