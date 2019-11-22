b53664cdeca69ff10e3602884421b23e456691cb
zhangting9756 <tingzhangguo@163.com>
2019-11-22 17:11:11 +0800

    增加数组代码

--- pythonForLeetcode/src/array.py
+++ pythonForLeetcode/src/array.py
@@ -69,6 +69,7 @@ class Solution6:
 """加一"""    
 class Solution7:
     def plusOne(self, digits):
+        """
         i=len(digits)-1
         carry=1
         while i>=0:
@@ -83,6 +84,17 @@ class Solution7:
         if carry==1:
             digits.insert(0,1)
         return digits
+        """
+        i=len(digits)-1
+        while i>0 and digits[i]==9:
+            digits[i]=0
+            i=i-1
+        if i==0 and digits[0]==9:
+            digits[0]=0
+            digits.insert(0,1)
+        else:
+            digits[i]=digits[i]+1
+        return digits
 """ 移动零 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。"""
 class Solution8:
     def moveZeroes(self, nums):
@@ -92,6 +104,7 @@ class Solution8:
                 nums.remove(i)
                 nums.append(i)
         """
+        """
         count, length  = 0, len(nums)
         while count != length:
             if nums[count] == 0:
@@ -99,12 +112,49 @@ class Solution8:
                 nums.append(0)
                 count -= 1
                 length -= 1
-            count += 1
-               
+            count += 1   
+        """
+        idxs = [idx for idx , num in enumerate(nums) if num == 0]
+        for i in idxs[::-1]:
+            nums.pop(i)
+        nums += len(idxs) *[0]
+"""两数之和 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。"""
+class Solution9:
+    def twoSum(self, nums, target):
+        dict = {}
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in dict:
+                return dict[a],i
+            else:
+                dict[nums[i]] = i 
+
+class Solution10:
+    def rotate(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        length = len(matrix)
+        for i in range(length):
+            for j in range(i+1,length):
+                temp = matrix[i,j]
+                matrix[i,j] = matrix[j,i]
+                matrix[j,i] = temp
+        for i in range(length):
+            matrix[i] = matrix[i][::-1]
+
+class Solution11:
+    def reverseString(self, s):
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
 if __name__ == '__main__':
-    a=[9,0,2,1,0,3,4,0]
-    c=Solution8()
-    c.moveZeroes(a)
+    a=["h","e","l","l","o"]
+    c=Solution11()
+    c.reverseString(a)
     print(a)
     
     
