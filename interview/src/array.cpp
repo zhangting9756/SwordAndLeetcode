db4fbc7225e753ddb5961d93eb167de2048f18d3
zhangting9756 <tingzhangguo@163.com>
2020-03-15 20:53:25 +0800

    增加求岛屿最大面积，深度遍历

--- interview/src/array.cpp
+++ interview/src/array.cpp
@@ -1012,3 +1012,40 @@ int lengthOfLIS(int* nums, int numsSize){
 	}*/
 	return key;
 }
+/*岛屿的最大面积
+给定一个包含了一些 0 和 1的非空二维数组 grid , 一个 岛屿 是由四个方向 (水平或垂直) 的 1 (代表土地) 构成的组合。你可以假设二维矩阵的四个边缘都被水包围着。
+找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0)*/
+void dfs(int **grid, int m, int n, int i, int j, int *cnt) {
+	if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) {
+		return;
+	}
+	/* 岛屿数量加1, 标记访问过的岛屿, 不会重复访问 */
+	(*cnt)++;
+	grid[i][j] = 2;
+	/* 上下左右寻找连在一起的岛屿 */
+	dfs(grid, m, n, i + 1, j, cnt);
+	dfs(grid, m, n, i - 1, j, cnt);
+	dfs(grid, m, n, i, j + 1, cnt);
+	dfs(grid, m, n, i, j - 1, cnt);
+	return;
+}
+int maxAreaOfIsland(int** grid, int gridSize, int* gridColSize){
+	if (gridSize == 0 || gridColSize[0] == 0) {
+		return 0;
+	}
+	int m = gridSize;
+	int n = gridColSize[0];
+	int cnt, i, j;
+	int max = 0;
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			/* 访问每一个小岛, 清零cnt, 寻找最大值 */
+			if (grid[i][j] == 1) {
+				cnt = 0;
+				dfs(grid, m, n, i, j, &cnt);
+				max = MAX(max, cnt);
+			}
+		}
+	}
+	return max;
+}
